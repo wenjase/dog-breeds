2d0ece5e5381917c7b03cdbabe7225286a13319e
alec2zac <alecjohnson708@gmail.com>
2021-07-29 17:00:22 -0500

    got my dog breeds in and the pictures all in

---
+++ script.js
@@ -0,0 +1,11 @@
+let breeds = ["border collie", "golden chi", "beagle", "german shepherd", "pomeranian", "rottweiler", "pit bull", "bull terrier", "bulldog", "chihuahua", "pug", "dobermann", "boxer", "great dane", "shih tzu", "corgi", "greyhound", "basset hound", "bernese mountain dog", "dachshund"]
+
+
+for (let i = 0; i < breeds.length; i++) {
+    
+}
+
+
+
+// let len = breeds.length
+// console.log(len)
