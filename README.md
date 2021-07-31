4599392b3e874b1ac6a0fd395e6de956f22b6c72
alec2zac <alecjohnson708@gmail.com>
2021-07-30 20:03:25 -0500

    figured out how i need to have my images show up for my flash cards

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dog-breeds
+
+<!-- today I want to make sure i get all my images working in a slideshow fashion. If I finish that I will go to work on the functionality of my project. -->
--- script.js
+++ script.js
@@ -1,9 +1,90 @@
-let breeds = ["border collie", "golden chi", "beagle", "german shepherd", "pomeranian", "rottweiler", "pit bull", "bull terrier", "bulldog", "chihuahua", "pug", "dobermann", "boxer", "great dane", "shih tzu", "corgi", "greyhound", "basset hound", "bernese mountain dog", "dachshund"]
+// got my array of dog breeds
+let breeds = ["border collie", 
+                "golden chi", 
+                "beagle", 
+                "german shepherd", 
+                "pomeranian", 
+                "rottweiler", 
+                "pit bull", 
+                "bull terrier", 
+                "bulldog", 
+                "chihuahua", 
+                "pug", 
+                "dobermann", 
+                "boxer", 
+                "great dane", 
+                "shih tzu", 
+                "corgi", 
+                "greyhound", 
+                "basset hound", 
+                "bernese mountain dog", 
+                "dachshund"]
 
+// all my documents are here.
+let img = document.querySelectorAll("#dogImage");
+let images = document.querySelectorAll('img')
+let rButton = document.querySelector('.rightButton')
+let lButton = document.querySelector('.leftButton')
 
-for (let i = 0; i < breeds.length; i++) {
-    
+// function to display and not display my animals
+function dogImages() {
+    images[0].style.display = 'block'
+    if (images[0] !== images[0]) {
+        images[0].style.display = 'none'
+    }
 }
+
+dogImages();
+
+// getting my left button to work so i can go to the next dog.
+function button() {
+    if ()
+}
+
+lButton.addEventListener('click', lButton)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function slideShow() {
+//     if (breeds === img){
+//         return 'correct'
+//     } else {
+//         return ''
+//     }
+// }
 
 
 
